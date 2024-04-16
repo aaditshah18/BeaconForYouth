5173416cb65a2db40a18f6552836e891ecf225a5
Aadit Shah <shah.aadit1@northeastern.edu>
2024-04-16 15:39:26 -0400

    Structure added for users directory

--- youth-devs-webservice/app/routes/index.js
+++ youth-devs-webservice/app/routes/index.js
@@ -1,13 +1,10 @@
 import complaintsRouter from "./complaints.js";
 import ngoRouter from "./ngo.js";
 
-
 const initializeRoutes = (app) => {
   app.use("/complaints", complaintsRouter);
   app.use("/ngos", ngoRouter);
+  app.use("/users", userRouter);
 };
 
-
 export default initializeRoutes;
-
-
---
+++ youth-devs-webservice/app/routes/users.js
@@ -0,0 +1,9 @@
+import express from "express";
+import * as userController from "../controllers/users.js";
+
+const router = express.Router();
+
+// User related APIs
+router.route("/login").post(userController.post);
+
+export default router;
