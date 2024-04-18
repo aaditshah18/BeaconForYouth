14e9f20c8876bfa2c1c95d6b3bcd7bc19cb0f242
Sohni Rais <rais.s@northeastern.edu>
2024-04-18 15:08:40 -0400

    Added complaint form

--- safe-harbour-youth-app/src/App.tsx
+++ safe-harbour-youth-app/src/App.tsx
@@ -42,3 +42,60 @@ function App() {
 }
 
 export default App;
+
+// import { ThemeProvider } from "@mui/system";
+// import "./App.css";
+// import { useEffect } from "react";
+// import { Container } from "@mui/material";
+// import theme from "./styles/themes";
+// import Appbar from "./components/header/appbar";
+// import AppDrawer from "./components/header/drawer";
+// import { UIProvider } from "./components/header/ui-context";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Footer from "./components/Footer/footer";
+// import Slider from "./components/Slider/hero-slider";
+// import AboutUs from "./components/AboutUs/about-us";
+// import CampaignSection from "./components/campaignsec";
+// import NGOLists from "./components/NGOLists";
+// import TrackComplaint from "./components/trackComplaint";
+// import ComplaintForm from "./components/complaintform";
+
+// function App() {
+//   useEffect(() => {
+//     document.title = "SafeHarbourYouth";
+//   }, []);
+
+//   return (
+//     <ThemeProvider theme={theme}>
+//       <Router>
+//         <Container maxWidth="xl" sx={{ background: "#fff" }}>
+//           <Appbar />
+//           <Routes>
+//             <Route
+//               path="./components/Slider/hero-slider"
+//               element={<Slider />}
+//             />
+//             <Route path="./components/AboutUs/about-us" element={<AboutUs />} />
+//             <Route
+//               path="./components/campaignsec"
+//               element={<CampaignSection />}
+//             />
+//             <Route path="./components/NGOLists" element={<NGOLists />} />
+//             <Route
+//               path="./components/trackComplaint"
+//               element={<TrackComplaint />}
+//             />
+//             <Route
+//               path="./components/complaintform"
+//               element={<ComplaintForm />}
+//             />
+//             {/* Define more routes as needed */}
+//           </Routes>
+//           <Footer />
+//         </Container>
+//       </Router>
+//     </ThemeProvider>
+//   );
+// }
+
+// export default App;
