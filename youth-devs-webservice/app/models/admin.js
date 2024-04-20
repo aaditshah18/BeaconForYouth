1039595ef869c3fe3f987e286bcf63621d5573d3
Aadit Shah <shah.aadit1@northeastern.edu>
2024-04-20 18:16:59 -0400

    Fixed routes, models and utilities

--- youth-devs-webservice/app/models/admin.js
+++ youth-devs-webservice/app/models/admin.js
@@ -1,7 +1,55 @@
-// Directly import the model
-import UserModel from "./users.js";
+import mongoose from "mongoose";
 
-// Define Admin model, inheriting properties from UserModel
-const AdminModel = UserModel.discriminator("Admin", {});
+const Schema = mongoose.Schema;
+
+// Define User/NGO model
+const adminSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  createdTs: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  updatedTs: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    default: "pending",
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+});
+
+const AdminModel = mongoose.model("Admin", adminSchema);
 
 export default AdminModel;
