00cf23be1307df4f1f3c2c0867f875c2e59e939c
Sushma-Kunjangada-Arun <kunjangadaarun.s@northeastern.edu>
2024-04-20 18:07:37 -0400

    Adding registration page

--- safe-harbour-youth-app/src/router.ts
+++ safe-harbour-youth-app/src/router.ts
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Login from "./pages/login";
+import Register from "./pages/register";
 import ComplaintForm from "./components/complaintform";
 import ngodetail from "./components/ngoInnerpage/ngodetail";
 import CampaignsPage from "./components/resource/campaingdetail";
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
   {
     path: "/login",
     Component: Login,
+  },
+  {
+    path: "/register",
+    Component: Register,
   },
   {
     path: "/complaintform",
