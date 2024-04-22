cbc12a8972e781ea121b2c134f49a4f8af1c4272
Sohni Rais <rais.s@northeastern.edu>
2024-04-22 01:58:38 -0400

    Add routes for update, delete, get by id

--- youth-devs-webservice/app/routes/campaigns.js
+++ youth-devs-webservice/app/routes/campaigns.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { createCampaign, getCampaignById, getCampaigns} from "../controllers/campaigns.js";
+import { createCampaign, getCampaignById, getCampaigns, deleteCampaign, updateCampaign} from "../controllers/campaigns.js";
 
 const router = express.Router();
 
 // Route to create a new campaign
 router.post('/', createCampaign);
+
+router.put('/:id', updateCampaign);
+
+router.delete('/:id', deleteCampaign);
+
 
 // Route to update a campaign
 // router.put('/campaigns/:id', campaignController);
@@ -14,5 +19,7 @@ router.post('/', createCampaign);
 
 // Route to get all campaigns
 router.get('/', getCampaigns);
+
+router.get('/:id', getCampaignById);
 
 export default router;
