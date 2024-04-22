cac95ea4b9dd9040188d4f4f2c03603c39665354
Sohni Rais <rais.s@northeastern.edu>
2024-04-22 00:54:20 -0400

    Add campaign mongoose model

---
+++ youth-devs-webservice/app/models/campaigns.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const CampaignSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+});
+
+const Campaign = mongoose.model('Campaign', CampaignSchema);
+
+export default Campaign;
