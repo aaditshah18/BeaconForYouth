13e5935c84bf441c9e538bb1cf76a245317a18e2
Aadit Shah <shah.aadit1@northeastern.edu>
2024-04-22 22:23:30 -0400

    Login API navigation and set token

--- safe-harbour-youth-app/src/api/users.ts
+++ safe-harbour-youth-app/src/api/users.ts
@@ -1,4 +1,3 @@
-import Cookies from "js-cookie";
 const USER_TOKEN_NAME = "Token";
 
 export type loginPayload = {
@@ -7,24 +6,41 @@ export type loginPayload = {
 };
 
 export const login = async (payload: loginPayload) => {
-  const res = await fetch("/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  });
-  const { data, result, msg } = await res.json();
+  try {
+    const res = await fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
 
-  if (data && data.token) {
-    Cookies.set(USER_TOKEN_NAME, data.token);
+    if (!res.ok) {
+      // Handling non-2xx status codes
+      const errorData = await res.json(); // Assuming the server responds with JSON for errors
+      console.error("Login failed:", errorData.msg);
+      throw new Error(errorData.msg || "Login failed due to server error");
+    }
+
+    const data = await res.json();
+    console.log("data ", data);
+
+    if (data && data.token) {
+      console.log("Login successful");
+      // Storing the token in localStorage
+      localStorage.setItem(USER_TOKEN_NAME, data.token);
+    } else {
+      console.log(data);
+      throw new Error("msg");
+    }
+
+    return {
+      // result,
+      // msg,
+      // data,
+    };
+  } catch (error) {
+    console.error("An error occurred during login:", error);
+    throw error; // Re-throwing the error after logging it, to handle it further up the call chain
   }
-
-  if (!result) {
-    throw new Error(msg);
-  }
-
-  return {
-    msg,
-  };
 };
